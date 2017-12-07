5c0d9a69febdd66340b3612f9616177c993df871
SIGMA SYD <fbarcos8@hotmail.com>
2017-12-07 11:52:54 -0500

    PlatziMusic en VUE js

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+  appname: 'platzimusic',
+  apiKey: '78d9db013c59dd1e1f59cc0240ccaf75',
+  secret: '3bdb9e5e5eb5105fcce06765899f6920',
+  registeredTo: 'sigmasyd128'
+}
+
+// Application name	platzimusic
+// API key	78d9db013c59dd1e1f59cc0240ccaf75
+// Shared secret	3bdb9e5e5eb5105fcce06765899f6920
+// Registered to	sigmasyd128
